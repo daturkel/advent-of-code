01e4dffd33c5b373ad85d94ff09236f39e339620
Dan Turkel <daturkel@gmail.com>
2023-12-01 12:45:42 -0500

    minor simplifications

--- 2023/01/01.py
+++ 2023/01/01.py
@@ -5,39 +5,36 @@ import sys
 from time import perf_counter
 
 NAME_TO_VALUE = {
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9,
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
 }
 
 
 def calibrate(lines: list[str]) -> tuple[int, int]:
-    values_one = []
-    values_two = []
+    first_sum = 0
+    second_sum = 0
     pattern_one = re.compile(r"\d")
     # this is a lookahead which handles overlaps like eightwo -> ["eight", "two"]:
     # (?=(\d|one|two|three|...))
     pattern_two = re.compile(r"(?=(\d|" + "|".join(NAME_TO_VALUE.keys()) + "))")
     for line in lines:
         digits_one = pattern_one.findall(line)
-        if digits_one:
-            first, last = int(digits_one[0]), int(digits_one[-1])
-        else:
-            first = last = 0
-        values_one.append(first * 10 + last)
+        first, last = digits_one[0], digits_one[-1]
+        first_sum += int(first + last)
 
         digits_two = pattern_two.findall(line)
         first, last = digits_two[0], digits_two[-1]
         first = NAME_TO_VALUE.get(first, first)
         last = NAME_TO_VALUE.get(last, last)
-        values_two.append(int(first) * 10 + int(last))
-    return sum(values_one), sum(values_two)
+        second_sum += int(first + last)
+    return first_sum, second_sum
 
 
 if __name__ == "__main__":
