0887eb503ce52f34e5243bc4466acf17743c0534
Dan Turkel <daturkel@gmail.com>
2023-12-02 09:05:30 -0500

    day 2

---
+++ 2023/02/02.py
@@ -0,0 +1,42 @@
+import sys
+from functools import reduce
+from operator import mul
+from time import perf_counter
+
+
+def get_possible_and_power(
+    games: list[str], max_per_color: dict[str, int]
+) -> tuple[int, int]:
+    total_possible = 0
+    total_power = 0
+    for i, game in enumerate(games):
+        game = game.split(": ", 1)[1]  # remove "Game N: " prefix
+        valid_game = True
+        least_of_color = {"red": 0, "blue": 0, "green": 0}
+        for reveal in game.split("; "):  # split into rounds
+            for color in reveal.split(", "):  # split into color-number pairs
+                num, color = color.split(" ")  # split color from number
+                num = int(num)
+                if num > least_of_color[color]:
+                    least_of_color[color] = num
+                if num > max_per_color[color]:
+                    valid_game = False
+        if valid_game:
+            total_possible += i + 1  # assumption that game # is always sequential
+        total_power += reduce(mul, least_of_color.values())
+    return total_possible, total_power
+
+
+if __name__ == "__main__":
+    input_file = sys.argv[1] if len(sys.argv) > 1 else "./test.txt"
+    tic = perf_counter()
+    with open(input_file, "r") as file:
+        lines = file.read().splitlines()
+
+    id_sum, power_sum = get_possible_and_power(
+        lines, {"red": 12, "blue": 14, "green": 13}
+    )
+    toc = perf_counter()
+    time_us = round((toc - tic) * 1000000)
+
+    print(f"{id_sum=}, {power_sum=} ({time_us}µs)")
