4298307d1e27606a74735dbb3e03eb89563b775a
Dan Turkel <daturkel@gmail.com>
2023-12-06 08:15:08 -0500

    re-solve day 6

--- 2023/06/06.py
+++ 2023/06/06.py
@@ -2,6 +2,28 @@ import sys
 from time import perf_counter
 
 
+def get_num_solutions(time: int, record: int) -> int:
+    """We want to know how many integral solutions there are to
+    (time - charge)*charge ≥ record
+    So we count the number of integers between the roots of the rewritten quadratic equation:
+    -1 * charge^2 + time * charge - record = 0"""
+    a = -1
+    b = time
+    c = -1 * record
+    neg_b = -1 * b
+    discriminant = (b**2 - 4 * a * c) ** (0.5)
+    denominator = 2 * a
+    root_1 = (neg_b + discriminant) / denominator
+    root_2 = (neg_b - discriminant) / denominator
+    # round up for the lower root
+    if root_1 != int(root_1):
+        root_1 = int(root_1 + 1)
+    # round down for the higher root
+    root_2 = int(root_2)
+    # return number of solutions
+    return root_2 - root_1 + 1
+
+
 def get_ways_to_win(lines: list[str]) -> tuple[int, int]:
     times = [int(num) for num in lines[0].split()[1:]]
     records = [int(num) for num in lines[1].split()[1:]]
@@ -19,11 +41,8 @@ def get_ways_to_win(lines: list[str]) -> tuple[int, int]:
     new_time = int("".join(str(num) for num in times))
     new_record = int("".join(str(num) for num in records))
     new_ways_to_win = 0
-    for charge in range(1, new_time):
-        time_left = new_time - charge
-        dist_achieved = charge * time_left
-        if dist_achieved > new_record:
-            new_ways_to_win += 1
+
+    new_ways_to_win = get_num_solutions(new_time, new_record)
 
     return total_a, new_ways_to_win
 
@@ -36,6 +55,6 @@ if __name__ == "__main__":
 
     ways_to_win, num_cards = get_ways_to_win(lines)
     toc = perf_counter()
-    time_us = round((toc - tic), 1)
+    time_us = round((toc - tic) * 1000000)
 
-    print(f"{ways_to_win=}, {num_cards=} ({time_us}s)")
+    print(f"{ways_to_win=}, {num_cards=} ({time_us}µs)")
