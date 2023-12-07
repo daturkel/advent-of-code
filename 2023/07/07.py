8a9e8976930d9749f7325bfe69912dfb42c30d03
Dan Turkel <daturkel@gmail.com>
2023-12-07 08:28:20 -0500

    day 7 part 2

--- 2023/07/07.py
+++ 2023/07/07.py
@@ -22,36 +22,74 @@ NEW_VALUE_MAP = VALUE_MAP | {"J": 0}
 
 
 def get_winnings(lines: list[str]) -> tuple[int, int]:
-    total = 0
-    hands = []
+    total_a = 0
+    total_b = 0
+    hands_a = []
+    hands_b = []
     for line in lines:
         hand, bet = line.split()
         bet = int(bet)
-        hand = [VALUE_MAP[char] for char in hand]
         counts = Counter(hand)
         sorted_counts = sorted(counts.values())
-        if sorted_counts == [5]:
-            hand_type = 7
-        elif sorted_counts == [1, 4]:
-            hand_type = 6
-        elif sorted_counts == [2, 3]:
-            hand_type = 5
-        elif sorted_counts == [1, 1, 3]:
-            hand_type = 4
-        elif sorted_counts == [1, 2, 2]:
-            hand_type = 3
-        elif sorted_counts == [1, 1, 1, 2]:
-            hand_type = 2
-        else:
-            hand_type = 1
-        hand = [hand_type] + hand
-        hands.append((hand, bet))
+        hand_a = [VALUE_MAP[char] for char in hand]
+        hand_b = [NEW_VALUE_MAP[char] for char in hand]
+        if sorted_counts == [5]:  # five of a kind
+            hand_type_a = 7
+            hand_type_b = 7
+        elif sorted_counts == [1, 4]:  # four of a kind
+            hand_type_a = 6
+            if counts["J"] > 0:  # whether we have 1 or 4 Js, upgrade to five of a kind
+                hand_type_b = 7
+            else:
+                hand_type_b = 6
+        elif sorted_counts == [2, 3]:  # full house
+            hand_type_a = 5
+            if counts["J"] > 0:  # whether we have 2 or 3 Js, upgrade to five of a kind
+                hand_type_b = 7
+            else:
+                hand_type_b = 5
+        elif sorted_counts == [1, 1, 3]:  # three of a kind
+            hand_type_a = 4
+            if counts["J"] > 0:  # whether we have 1 or 3 Js, upgrade to four of a kind
+                hand_type_b = 6
+            else:
+                hand_type_b = 4
+        elif sorted_counts == [1, 2, 2]:  # two pair
+            hand_type_a = 3
+            num_j = counts["J"]
+            if num_j == 1:  # one J upgrades to full house
+                hand_type_b = 5
+            elif num_j == 2:  # two Js upgrades to four of a kind
+                hand_type_b = 6
+            else:
+                hand_type_b = 3
+        elif sorted_counts == [1, 1, 1, 2]:  # one pair
+            hand_type_a = 2
+            num_j = counts["J"]
+            if num_j > 0:  # whether we have 1 or 2 Jokers, we get three of a kind
+                hand_type_b = 4
+            else:
+                hand_type_b = 2
+        else:  # highest card
+            hand_type_a = 1
+            if counts["J"] == 1:  # if we have 1 J, upgrade to one pair
+                hand_type_b = 2
+            else:
+                hand_type_b = 1
+        hand_a = [hand_type_a] + hand_a
+        hand_b = [hand_type_b] + hand_b
+        hands_a.append((hand_a, bet))
+        hands_b.append((hand_b, bet))
 
-    sorted_hands = sorted(hands)
-    for rank, (_, bet) in enumerate(sorted_hands, start=1):
-        total += rank * bet
+    sorted_hands_a = sorted(hands_a)
+    for rank, (_, bet) in enumerate(sorted_hands_a, start=1):
+        total_a += rank * bet
 
-    return total, 0
+    sorted_hands_b = sorted(hands_b)
+    for rank, (_, bet) in enumerate(sorted_hands_b, start=1):
+        total_b += rank * bet
+
+    return total_a, total_b
 
 
 if __name__ == "__main__":
@@ -60,8 +98,8 @@ if __name__ == "__main__":
     with open(input_file, "r") as file:
         lines = file.read().splitlines()
 
-    winnings, num_cards = get_winnings(lines)
+    winnings, alt_winnings = get_winnings(lines)
     toc = perf_counter()
-    time_us = round((toc - tic) * 1000000)
+    time_us = round((toc - tic) * 1000)
 
-    print(f"{winnings=}, {num_cards=} ({time_us}µs)")
+    print(f"{winnings=}, {alt_winnings=} ({time_us}ms)")
