187d9f2df80c3c34a0b7d39870cc863a3f756ce9
Dan Turkel <daturkel@gmail.com>
2023-12-08 09:54:10 -0500

    day 8 part 2

--- 2023/08/08.py
+++ 2023/08/08.py
@@ -1,10 +1,23 @@
 import sys
+from math import lcm
 from time import perf_counter
 
 
-def get_steps(lines: list[str]) -> tuple[int, int]:
-    directions = [{"L": 0, "R": 1}[direction] for direction in lines[0]]
+def get_steps_til_end(
+    start: str, directions: list[int], graph: dict[str, tuple[str, str]]
+) -> int:
+    current_node = start
     n_directions = len(directions)
+    num_steps = 0
+    while current_node[2] != "Z":
+        num_steps += 1
+        direction = directions[(num_steps - 1) % n_directions]
+        current_node = graph[current_node][direction]
+    return num_steps
+
+
+def solve(lines: list[str]) -> tuple[int, int]:
+    directions = [{"L": 0, "R": 1}[direction] for direction in lines[0]]
     graph = {}
     for line in lines[2:]:
         node = line[:3]
@@ -12,16 +25,18 @@ def get_steps(lines: list[str]) -> tuple[int, int]:
         right = line[12:15]
         graph[node] = [left, right]
 
-    num_steps = 0
-    current_node = "AAA"
-    visited = [current_node]
-    while current_node != "ZZZ":
-        num_steps += 1
-        direction = directions[(num_steps - 1) % n_directions]
-        current_node = graph[current_node][direction]
-        visited.append(current_node)
+    starts = [node for node in graph.keys() if node[2] == "A"]
+    cycle_lengths = []
+    num_steps_a = 0
+    for start in starts:
+        length = get_steps_til_end(start, directions, graph)
+        cycle_lengths.append(length)
+        if start == "AAA":
+            num_steps_a = length
 
-    return num_steps, 0
+    num_steps_b = lcm(*cycle_lengths)
+
+    return num_steps_a, num_steps_b
 
 
 if __name__ == "__main__":
@@ -30,8 +45,8 @@ if __name__ == "__main__":
     with open(input_file, "r") as file:
         lines = file.read().splitlines()
 
-    num_steps, alt_winnings = get_steps(lines)
+    num_steps, alt_num_steps = solve(lines)
     toc = perf_counter()
     time_us = round((toc - tic) * 1000)
 
-    print(f"{num_steps=}, {alt_winnings=} ({time_us}ms)")
+    print(f"{num_steps=}, {alt_num_steps=} ({time_us}ms)")
