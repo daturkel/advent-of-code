3ef07e131976696cb6896fdb325c3adeb1f3cc38
Dan Turkel <daturkel@gmail.com>
2023-12-10 10:42:47 -0500

    almost 10p2

--- 2023/10/10.py
+++ 2023/10/10.py
@@ -21,6 +21,67 @@ for piece, dirs in PIECE_TO_DIRS.items():
 
 DIR_TO_DXDY = {"N": (0, -1), "S": (0, 1), "E": (1, 0), "W": (-1, 0)}
 
+Pair = tuple[int, int]
+SetOfPairs = set[Pair]
+
+
+def check_point(
+    x: int,
+    y: int,
+    border: SetOfPairs,
+    outside: SetOfPairs,
+    inside: SetOfPairs,
+    lines: list[str],
+    width: int,
+    height: int,
+) -> tuple[SetOfPairs, SetOfPairs]:
+    point = (x, y)
+    dw = x
+    dn = y
+    de = width - x
+    ds = height - y
+    nearest = min(dw, dn, de, ds)
+    if nearest == dw:
+        dir_ = "W"
+    elif nearest == dn:
+        dir_ = "N"
+    elif nearest == de:
+        dir_ = "E"
+    else:
+        dir_ = "S"
+    print(f"piece={lines[y][x]}, {dir_=}")
+    dx, dy = DIR_TO_DXDY[dir_]
+    print("---")
+    num_crosses = 0
+    while True:
+        x, y = x + dx, y + dy
+        if (0 <= x < width) and (0 <= y < height):
+            neighbor = lines[y][x]
+        else:
+            if num_crosses % 2 == 0:
+                print(f"edge, {num_crosses=}, outside")
+                outside.add(point)
+            else:
+                print(f"edge, {num_crosses=}, inside")
+                inside.add(point)
+            return outside, inside
+
+        if ((x, y) in outside) and (num_crosses % 2 == 0):
+            print(f"{neighbor=} outside, outside")
+            outside.add(point)
+            return outside, inside
+        elif ((x, y) in inside) and (num_crosses == 0):
+            print(f"{neighbor=} inside, inside")
+            inside.add(point)
+            return outside, inside
+        elif (x, y) in border:
+            print(f"{neighbor=} border, {dir_=}")
+            if (dir_ not in PIECE_TO_DIRS[neighbor]) and (
+                neighbor not in DIR_TO_VALID_PIECES[dir_]
+            ):
+                print(f"{neighbor=} border, {dir_=}, crossing")
+                num_crosses += 1
+
 
 def solve(raw_lines: str) -> tuple[int, int]:
     # --------------------------- figure out starting point -------------------------- #
@@ -30,6 +91,7 @@ def solve(raw_lines: str) -> tuple[int, int]:
     sx = (si - sy) % width  # get x coordinate
     lines = raw_lines.split("\n")
     starting_dirs = set([])
+    starting_piece = "S"
     for dir_ in "SENW":
         dx, dy = DIR_TO_DXDY[dir_]
         try:
@@ -38,6 +100,11 @@ def solve(raw_lines: str) -> tuple[int, int]:
                 starting_dirs.add(dir_)
         except IndexError:
             continue
+    for piece, dirs in PIECE_TO_DIRS.items():
+        if dirs == starting_dirs:
+            starting_piece = piece
+            break
+    lines[sy] = lines[sy].replace("S", starting_piece)
     # --------------------------------- navigate maze -------------------------------- #
     x, y = sx, sy
     dir_ = list(starting_dirs)[0]  # pick arbitrary starting direction
@@ -54,7 +121,27 @@ def solve(raw_lines: str) -> tuple[int, int]:
         distances[(x, y)] = distance
 
     max_distance = int((max(distances.values()) + 1) / 2)
-    return max_distance, 0
+    # ------------------------------------ part 2 ------------------------------------ #
+    border = set(distances.keys())
+    outside = set([])
+    inside = set([])
+    height = len(lines)
+
+    for x in range(width):
+        for y in range(height):
+            print(f"point={(x, y)}, piece={lines[y][x]}")
+            if (x, y) in border:
+                print("border")
+                continue
+            else:
+                outside, inside = check_point(
+                    x, y, border, outside, inside, lines, width, height
+                )
+
+    num_inside = len(inside)
+    print(inside)
+
+    return max_distance, num_inside
 
 
 if __name__ == "__main__":
