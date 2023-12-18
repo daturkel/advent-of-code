90b103f7d5cf03079ac583689de365f9ce9cb54d
Dan Turkel <daturkel@gmail.com>
2023-12-17 21:40:00 -0500

    optimizations

--- 2023/17/17.py
+++ 2023/17/17.py
@@ -1,13 +1,13 @@
 from __future__ import annotations
 
 import sys
-from collections import defaultdict
 from heapq import heappop, heappush
 from itertools import count
 from time import perf_counter
-from typing import Callable, NamedTuple
+from typing import Callable
 
 Point = tuple[int, int]
+Node = tuple[Point, str, int]
 
 
 class PriorityQueue:
@@ -29,7 +29,7 @@ class PriorityQueue:
 
     def pop(self):
         while self._pq:
-            priority, _, node = heappop(self._pq)
+            _, _, node = heappop(self._pq)
             if node is not None:
                 del self.lookup[node]
                 return node
@@ -39,49 +39,43 @@ class PriorityQueue:
         return self.lookup[node][0]
 
 
-# point, direction we're moving, and steps from that direction
-class Node(NamedTuple):
-    point: Point
-    direction: str
-    steps: int
-
-
-DIRECTIONS = {"E": (1, 0), "W": (-1, 0), "N": (0, -1), "S": (0, 1)}
+DIRECTIONS = [("E", (1, 0)), ("W", (-1, 0)), ("N", (0, -1)), ("S", (0, 1))]
 OPPOSITES = {"E": "W", "W": "E", "N": "S", "S": "N", "": ""}
+INF = float("inf")
 
 
 def get_neighbors_a(node: Node, width: int, height: int) -> list[Node]:
-    x, y = node.point
+    x, y = node[0]
     neighbors = []
 
-    for direction, (dx, dy) in DIRECTIONS.items():
-        if direction == OPPOSITES[node.direction]:
+    for direction, (dx, dy) in DIRECTIONS:
+        if direction == OPPOSITES[node[1]]:
             continue
         if not (0 <= x + dx < width) or not (0 <= y + dy < height):
             continue
-        if (direction == node.direction) and (node.steps < 3):
-            neighbors.append(Node((x + dx, y + dy), direction, node.steps + 1))
-        if direction != node.direction:
-            neighbors.append(Node((x + dx, y + dy), direction, 1))
+        if (direction == node[1]) and (node[2] < 3):
+            neighbors.append(((x + dx, y + dy), direction, node[2] + 1))
+        if direction != node[1]:
+            neighbors.append(((x + dx, y + dy), direction, 1))
 
     return neighbors
 
 
 def get_neighbors_b(node: Node, width: int, height: int) -> list[Node]:
-    x, y = node.point
+    x, y = node[0]
     neighbors = []
 
-    for direction, (dx, dy) in DIRECTIONS.items():
-        if direction == OPPOSITES[node.direction]:
+    for direction, (dx, dy) in DIRECTIONS:
+        if direction == OPPOSITES[node[1]]:
             continue
         if not (0 <= x + dx < width) or not (0 <= y + dy < height):
             continue
-        if (direction == node.direction) and (node.steps < 10):
-            neighbors.append(Node((x + dx, y + dy), direction, node.steps + 1))
-        elif (direction != node.direction) and (node.steps >= 4):
-            neighbors.append(Node((x + dx, y + dy), direction, 1))
-        elif node.direction == "":
-            neighbors.append(Node((x + dx, y + dy), direction, 1))
+        if (direction == node[1]) and (node[2] < 10):
+            neighbors.append(((x + dx, y + dy), direction, node[2] + 1))
+        elif (direction != node[1]) and (node[2] >= 4):
+            neighbors.append(((x + dx, y + dy), direction, 1))
+        elif node[1] == "":
+            neighbors.append(((x + dx, y + dy), direction, 1))
 
     return neighbors
 
@@ -93,23 +87,23 @@ def dijkstra(
     neighbor_fn: Callable[[Node, int, int], list[Node]],
     min_stop: int = 0,
 ) -> int:
-    distances = defaultdict(lambda: float("inf"))
+    distances = {}
     pq = PriorityQueue()
     width = len(grid[0])
     height = len(grid)
-    current_node = Node(start, "", 0)
-    pq.add_update(current_node, float("inf"))
+    current_node = (start, "", 0)
+    pq.add_update(current_node, INF)
     distances[current_node] = 0
 
     while True:
-        current_distance = distances[current_node]
+        current_distance = distances.get(current_node, INF)
         for neighbor_node in neighbor_fn(current_node, width, height):
-            nx, ny = neighbor_node.point
+            nx, ny = neighbor_node[0]
             possible_distance = current_distance + grid[ny][nx]
-            if possible_distance < distances[neighbor_node]:
+            if possible_distance < distances.get(neighbor_node, INF):
                 distances[neighbor_node] = possible_distance
                 pq.add_update(neighbor_node, possible_distance)
-                if ((nx, ny) == end) and (neighbor_node.steps > min_stop):
+                if ((nx, ny) == end) and (neighbor_node[2] > min_stop):
                     return int(possible_distance)
         current_node = pq.pop()
 
@@ -130,8 +124,8 @@ if __name__ == "__main__":
     with open(input_file, "r") as file:
         lines = file.read().splitlines()
 
-    min_heat_loss, new_ways_to_win = solve(lines)
+    min_heat_loss, min_heat_loss_ultra = solve(lines)
     toc = perf_counter()
     time_us = round((toc - tic), 1)
 
-    print(f"{min_heat_loss=}, {new_ways_to_win=} ({time_us}s)")
+    print(f"{min_heat_loss=}, {min_heat_loss_ultra=} ({time_us}s)")
