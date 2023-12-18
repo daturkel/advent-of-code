f1ae97b12c5c4e8b0b07762ee78ae47175c6a276
Dan Turkel <daturkel@gmail.com>
2023-12-18 10:36:56 -0500

    much better solution to part 2

--- 2023/18/18.py
+++ 2023/18/18.py
@@ -2,106 +2,53 @@ import sys
 from time import perf_counter
 
 DIRECTIONS = {"R": (1, 0), "L": (-1, 0), "D": (0, 1), "U": (0, -1)}
-POINT_TYPE: dict[tuple[str, str], str] = {
-    ("D", "R"): "L",
-    ("D", "L"): "J",
-    ("U", "R"): "F",
-    ("U", "L"): "7",
-    ("R", "D"): "7",
-    ("R", "U"): "J",
-    ("L", "D"): "F",
-    ("L", "U"): "L",
-}
-BORDER_CROSSINGS = {
-    ("L", "7"),
-    ("7", "L"),
-    ("|", "|"),
-    ("-", "-"),
-    ("F", "J"),
-    ("J", "F"),
-}
-BORDERS_GOING_EAST = {"L", "F", "-"}
 HEX_TO_DIRECTION = {"0": "R", "1": "D", "2": "L", "3": "U"}
 
 
-def get_grid(
-    instructions: list[tuple[str, int]]
-) -> tuple[dict[tuple[int, int], str], int, int]:
-    points = [(0, 0)]
-    x, y = points[0]
+def get_len_border_and_area(instructions: list[tuple[str, int]]) -> tuple[int, int]:
+    len_border = 1
+    x, y = 0, 0
+    old_x, old_y = 0, 0
     n_lines = len(instructions)
-    point_lookup = {}
+    area = 1
     for i in range(n_lines):
-        last_direction = instructions[(i - 1) % n_lines][0]
         direction, amount = instructions[i]
         dx, dy = DIRECTIONS[direction]
         amount = int(amount)
-        point_lookup[(x, y)] = POINT_TYPE[(last_direction, direction)]
         for i in range(1, amount + 1):
+            len_border += 1
+            old_x = x
+            old_y = y
             x = x + dx
             y = y + dy
-            if direction in ("L", "R"):
-                point_lookup[(x, y)] = "-"
-            else:
-                point_lookup[(x, y)] = "|"
-            points.append((x, y))
-    point_lookup[(0, 0)] = POINT_TYPE[(instructions[-1][0], instructions[0][0])]
+            # who knew! https://en.wikipedia.org/wiki/Shoelace_formula#Trapezoid_formula
+            area += (old_y + y) * (old_x - x)
 
-    xs = [point[0] for point in points]
-    ys = [point[1] for point in points]
-    xmin = min(xs)
-    ymin = min(ys)
-    xmax = max(xs)
-    ymax = max(ys)
-    width = xmax - xmin + 1
-    height = ymax - ymin + 1
-
-    grid = {}
-    for i, (x, y) in enumerate(points):
-        grid[(x - xmin, y - ymin)] = point_lookup[(x, y)]
-
-    return grid, width, height
+    # len_border - 1 to remove duplicate first point
+    return len_border - 1, int(area / 2)
 
 
-def get_filled_grid_size(grid: dict[tuple[int, int], str], width, height) -> int:
-    total = 0
-    for y in range(height):
-        inside = False
-        in_border = False
-        border_start = ""
-        border_end = ""
-        for x in range(width):
-            char = grid.get((x, y), ".")
-            if char != ".":
-                total += 1
-                if not in_border:
-                    border_start = char
-                    in_border = True
-                if in_border and char not in BORDERS_GOING_EAST:
-                    border_end = char
-                    in_border = False
-                    if (border_start, border_end) in BORDER_CROSSINGS:
-                        inside = not inside
-            elif inside:
-                total += 1
-
-    return total
+def get_volume(area: int, num_boundary: int) -> int:
+    # Really helpful! https://en.wikipedia.org/wiki/Pick%27s_theorem
+    num_interior = int(area + 1 - num_boundary / 2)
+    return num_boundary + num_interior
 
 
 def solve(lines: list[str]) -> tuple[int, int]:
     instructions_a = [(line.split()[0], int(line.split()[1])) for line in lines]
-    grid, width, height = get_grid(instructions_a)
-    filled_grid_size = get_filled_grid_size(grid, width, height)
-    # instructions_b = []
-    # for line in lines:
-    #     hex_part = line.split("#")[1]
-    #     amount = int(hex_part[:-2], 16)
-    #     direction = HEX_TO_DIRECTION[hex_part[-2]]
-    #     instructions_b.append((direction, amount))
-    # grid, width, height = get_grid(instructions_b)
-    # filled_grid_size = get_filled_grid_size(grid, width, height)
+    len_border_a, area_a = get_len_border_and_area(instructions_a)
+    volume_a = get_volume(area_a, len_border_a)
 
-    return filled_grid_size, 0
+    instructions_b = []
+    for line in lines:
+        hex_part = line.split("#")[1]
+        amount = int(hex_part[:-2], 16)
+        direction = HEX_TO_DIRECTION[hex_part[-2]]
+        instructions_b.append((direction, amount))
+    len_border_b, area_b = get_len_border_and_area(instructions_b)
+    volume_b = get_volume(area_b, len_border_b)
+
+    return volume_a, volume_b
 
 
 if __name__ == "__main__":
@@ -110,8 +57,8 @@ if __name__ == "__main__":
     with open(input_file, "r") as file:
         lines = file.read().splitlines()
 
-    min_heat_loss, min_heat_loss_ultra = solve(lines)
+    volume_a, volume_b = solve(lines)
     toc = perf_counter()
     time_us = round((toc - tic), 1)
 
-    print(f"{min_heat_loss=}, {min_heat_loss_ultra=} ({time_us}s)")
+    print(f"{volume_a=}, {volume_b=} ({time_us}s)")
--- 2023/18/notes.md
+++ 2023/18/notes.md
@@ -1,1 +1,9 @@
 For today, I copied my logic from day 10 for detecting inner and outer pieces, though instead of starting at a point and heading outward, I just scan left to right on each row. This works, but it's way too slow for part 2.
+
+---
+
+Second attempt, I found a reddit comment mentioning using the shoelace formula and pick's theorem. I had never heard of either of them but they make the problem trivial.
+
+One weird thing is that I encountered this issue for the first time while annotating `get_border`: https://stackoverflow.com/a/76765708 (though this doesn't remain in the final solution)
+
+I reduced time from 52 seconds to 23 seconds by calculating the length of the border and calculating the area simultaneously, rather than building up a list of border pieces and calculating area after. This way we avoid expensive calls to list.append.
