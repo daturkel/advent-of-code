951e8c1db95eeb4993c11396430f7601edb341f7
Dan Turkel <daturkel@gmail.com>
2024-12-01 09:02:58 -0500

    updte note; add template

---
+++ 2024/template.py
@@ -0,0 +1,19 @@
+import sys
+from time import perf_counter
+
+
+def solve(lines: list[str]) -> tuple[int, int]:
+    return 0, 0
+
+
+if __name__ == "__main__":
+    input_file = sys.argv[1] if len(sys.argv) > 1 else "./test.txt"
+    tic = perf_counter()
+    with open(input_file, "r") as file:
+        lines = file.readlines()
+
+    part_one, part_two = solve(lines)
+    toc = perf_counter()
+    time_us = round((toc - tic) * 1000)
+
+    print(f"{part_one=}, {part_two=} ({time_us}ms)")
