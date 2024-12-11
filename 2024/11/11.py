ea6c525e168b2bbbdf99902f7138972018f59e97
Dan Turkel <daturkel@gmail.com>
2024-12-11 08:45:20 -0500

    day 11

--- 2024/11/11.py
+++ 2024/11/11.py
@@ -2,24 +2,34 @@ import sys
 from time import perf_counter
 
 
+def blink(stone: int, times: int, cache: dict[tuple[int, int], int] = {}) -> int:
+    answer = 0
+    if (stone, times) in cache:
+        return cache[(stone, times)]
+    elif times == 0:
+        return 1
+    elif stone == 0:
+        answer = blink(1, times - 1)
+    elif len(str(stone)) % 2 == 0:
+        str_stone = str(stone)
+        left = int(str_stone[: len(str_stone) // 2])
+        right = int(str_stone[len(str_stone) // 2 :])
+        answer = blink(left, times - 1) + blink(right, times - 1)
+    else:
+        answer = blink(2024 * stone, times - 1)
+    cache[(stone, times)] = answer
+    return answer
+
+
 def solve(stones_str: str) -> tuple[int, int]:
     stones = [int(n) for n in stones_str.split()]
-    for _ in range(25):
-        new_stones = []
-        for stone in stones:
-            if stone == 0:
-                new_stones.append(1)
-            elif len(str(stone)) % 2 == 0:
-                str_stone = str(stone)
-                left = int(str_stone[: len(str_stone) // 2])
-                right = int(str_stone[len(str_stone) // 2 :])
-                new_stones += [left, right]
-            else:
-                new_stones.append(stone * 2024)
-        stones = new_stones
-        print(len(stones))
-        input()
-    return len(stones), 0
+    part_one = 0
+    part_two = 0
+    for stone in stones:
+        part_one += blink(stone, 25)
+        part_two += blink(stone, 75)
+
+    return part_one, part_two
 
 
 if __name__ == "__main__":
@@ -28,7 +38,7 @@ if __name__ == "__main__":
     with open(input_file, "r") as file:
         stones = file.read().strip()
 
-    part_one, part_two = solve("0")
+    part_one, part_two = solve(stones)
     toc = perf_counter()
     time_us = round((toc - tic) * 1000)
 
---
+++ 2024/11/notes.md
@@ -0,0 +1,5 @@
+We need to track a system of cellular automata after 25 and after 75 steps. After 25 is reasonable to do manually applying the rules (my original solution for part 1), but 75 grows way too big.
+
+To make this tractable for part 2, I replaced a for loop with a recursive function with a cache. This takes advantage of the fact that many numbers occur many times.
+
+My cache is keyed on (number, units of time remaining), so it only has cache hits for duplicate numbers in the same iteration. Another way to do this would be to track the result for each starting number after N units of time have *passed*. This way, if you encounter 0 in the first time step, you'd get cache entries for (0, 75), (0, 74), (0, 73...) that you could use at every future time stamp whenever you encounter a 0. However, with part two working in ~65ms, this optimization was not needed.
