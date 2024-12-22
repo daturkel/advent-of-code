208e783f3caf76a67735e5259f98a22e53c2a910
Dan Turkel <daturkel@gmail.com>
2024-12-22 13:39:35 -0500

    minor optimization

--- 2024/22/22.py
+++ 2024/22/22.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 from time import perf_counter
 
 
@@ -22,38 +23,26 @@ def step_three(n: int) -> int:
 
 def solve(lines: list[str]) -> tuple[int, int]:
     part_one = 0
-    changes = {i: [] for i in range(len(lines))}
-    digits = {i: [] for i in range(len(lines))}
     prices = {i: {} for i in range(len(lines))}
     all_combos_seen = set()
     for i, line in enumerate(lines):
+        buffer = deque([])
         n = int(line)
         first_digit_old = n % 10
+        combos_seen = set()
         for _ in range(2000):
-            digits[i].append(first_digit_old)
             tmp = step_three(step_two(step_one(n)))
             first_digit_new = tmp % 10
-            changes[i].append(first_digit_new - first_digit_old)
+            buffer.append(first_digit_new - first_digit_old)
+            if len(buffer) > 4:
+                buffer.popleft()
+                if tuple(buffer) not in combos_seen:
+                    combos_seen.add(tuple(buffer))
+                    prices[i][tuple(buffer)] = first_digit_new
             first_digit_old = first_digit_new
             n = tmp
-        digits[i].append(first_digit_old)
-        part_one += n
-        combos_seen = set()
-        for j in range(len(changes[i])):
-            if j < 3:
-                continue
-            combo = (
-                changes[i][j - 3],
-                changes[i][j - 2],
-                changes[i][j - 1],
-                changes[i][j],
-            )
-            if combo in combos_seen:
-                continue
-            else:
-                combos_seen.add(combo)
-                prices[i][combo] = digits[i][j + 1]
         all_combos_seen = all_combos_seen.union(combos_seen)
+        part_one += n
 
     most_bananas = 0
     for combo in all_combos_seen:
