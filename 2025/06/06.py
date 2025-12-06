04bd2721a51e08a5c6d9aa31caf6a80881342ced
Dan Turkel <daturkel@gmail.com>
2025-12-06 13:19:34 -0500

    day 6

--- 2025/06/06.py
+++ 2025/06/06.py
@@ -1,18 +1,54 @@
+import re
 import sys
 from math import prod
 from time import perf_counter
 
+OP_MAP = {"*": prod, "+": sum}
 
-def solve(lines: list) -> tuple[int, int]:
+
+def solve_alt(lines: list[str]) -> tuple[int, int]:
+    # alternative solution, part 1 only
     part_one = 0
-    part_two = 0
-    lines[:-1] = [map(int, line.split()) for line in lines[:-1]]
-    operand_list = zip(*lines[:-1])
+    processed_lines = [map(int, line.split()) for line in lines[:-1]]
+    operand_list = zip(*processed_lines)
     for operator, operands in zip(lines[-1].split(), operand_list):
         if operator == "+":
             part_one += sum(operands)
         else:
             part_one += prod(operands)
+    return part_one, 0
+
+
+def solve(lines: list[str]) -> tuple[int, int]:
+    part_one = 0
+    part_two = 0
+
+    num_operands = len(lines) - 1
+    # ["*   ", "+  ", ... ]
+    operators = re.findall(r"[\*\+]\s*", lines[-1])
+    for operator in operators:
+        # how wide is this equation
+        op_length = len(operator)
+        operands = []
+        # get all operands from each row, preserving whitespace
+        for j in range(num_operands):
+            num = lines[j][:op_length]
+            operands.append(num)
+            # chop off the start of the line
+            lines[j] = lines[j][op_length:]
+        operator = operator[:1]
+        # aggregate the operands normally for part 1
+        part_one += OP_MAP[operator](map(lambda x: int(x.strip()), operands))
+        operands_two = []
+        # transpose the operands for part 2, then aggregate
+        for j in range(op_length):
+            s = ""
+            for row in operands:
+                s += row[j].strip()
+            # ignore empty string at end of very last equation
+            if s:
+                operands_two.append(int(s))
+        part_two += OP_MAP[operator](operands_two)
 
     return part_one, part_two
 
